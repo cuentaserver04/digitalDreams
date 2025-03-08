009f97d6a4b4c19c4a5967b56cc672731ce491bd
cuentaserver04 <servidorescuenta34@gmail.com>
2025-03-08 14:00:54 -0500

    Configurando conexión con Railway

--- src/main/java/com/sena/_2/digitalDream/DigitalDreamApplication.java
+++ src/main/java/com/sena/_2/digitalDream/DigitalDreamApplication.java
@@ -3,8 +3,12 @@ package com.sena._2.digitalDream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.sena._2.digitalDream")
@@ -14,6 +18,19 @@ public class DigitalDreamApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DigitalDreamApplication.class, args);
+	}
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 
 }
